f5fb44ddcc9b54096eb8059c4232faa97d1cb6d2
Betül Zilan Tüzün <120500655+bzilan@users.noreply.github.com>
2024-08-17 22:39:56 +0300

    initial commit
    initial commit

---
+++ Bestcloud/WebApi/Controllers/BestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BestController : ControllerBase
+    {
+        
+        // GET /message
+        [HttpGet("/")]
+        public IActionResult GetMessage()
+        {
+            return Ok(new { msg = "BC4M" });
+        }
+
+        // GET /health
+        [HttpGet("/health")]
+        public IActionResult CheckHealthStatus()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+
+
+        // POST /
+        [HttpPost("/")]
+        public IActionResult PostData([FromBody] object data)
+        {
+            return Ok(data);
+        }
+    }
+}
